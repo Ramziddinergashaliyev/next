1a4790eb74a3d9b3c466f65ac01ccda03d01ad15
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-07-11 15:49:30 +0500

    added

--- src/components/default-values/DefaultValues.jsx
+++ src/components/default-values/DefaultValues.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { defaultCart } from "@/lib/features/cart/cartSlice";
-import { defaultCounter } from "@/lib/features/counter/counterSlice";
 import { wishlistLocal } from "@/lib/features/wishlist/wishlistSlice";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
