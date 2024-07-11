dabbea2534c472be2a16ecb64772ca13511f6516
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-07-11 16:07:32 +0500

    add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,15 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero__card"></div>
+      <div className="hero__card">
+        <span>🌱 </span>
+        <h3>The nature candle</h3>
+        <p>
+          All handmade with natural soy wax, Candleaf is a companion for all
+          your pleasure moments
+        </p>
+        <button>Discovery our collection</button>
+      </div>
     </div>
   );
 };
